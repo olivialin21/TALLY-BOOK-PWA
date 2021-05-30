42adfb24e1b521c71fa74880077d027b7da5a97d
olivialin21 <s110819023@stu.ntue.edu.tw>
2021-05-30 18:48:05 +0800

    workbox-recipes

--- src/components/MessageBoard.js
+++ src/components/MessageBoard.js
@@ -20,6 +20,22 @@ export default function MessageBoard() {
     firebase.initializeApp(firebaseConfig);
   }
 
+  const store = firebase.firestore();
+  store.enablePersistence()
+  .catch(function(err) {
+    if (err.code == 'failed-precondition') {
+        // Multiple tabs open, persistence can only be enabled
+        // in one tab at a a time.
+        // ...
+        console.log(err.code);
+    } else if (err.code == 'unimplemented') {
+        // The current browser does not support all of the
+        // features required to enable persistence
+        // ...
+        console.log(err.code);
+    }
+  });
+  
   const onClick = (e) => {
     e.preventDefault();
 
